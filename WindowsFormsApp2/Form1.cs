1cbea683806fe441064222abfc726d3112c5d074
Артём <71020677+NIVEX1362@users.noreply.github.com>
2021-01-16 16:57:09 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Добро пожаловать))");
+        }
+
+    
+          
+       
+        
+          private void button2_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.citilink.ru/catalog/computers_and_notebooks/parts/motherboards/?gclid=EAIaIQobChMIw9zRi4GP7gIVAt-yCh0d3QwHEAAYASAAEgI_4fD_BwE");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3();
+            f.Show();
+        }
+    }
+
+
+}
