05cc0e5ddfe1213a8b7d3bdb9dc885c4734e2ad4
Артём <71020677+NIVEX1362@users.noreply.github.com>
2021-02-20 16:47:58 +0300

    Add files via upload

--- WindowsFormsApp2/Form3.cs
+++ WindowsFormsApp2/Form3.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsN1VeX
-{
+{  
     public struct Detail
     {
         public PictureBox picture;
@@ -19,6 +19,15 @@ namespace WindowsN1VeX
         public string category;
         public string name;
         public string opisanie;
+        public Detail(string name1, string category1, int price1)
+        {
+            label = new Label();
+            picture = new PictureBox();
+            name = name1;
+            price = price1;
+            category = category1;
+            opisanie = "";
+        }
     }
     public partial class Form3 : Form
     {
@@ -28,69 +37,21 @@ namespace WindowsN1VeX
         {
             InitializeComponent();
 
+            details[0] = new Detail("Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1", "Корпуса", 0);
+            details[1] = new Detail("Корпус GiNZZU GL5501", "Корпуса", 0);
+            details[2] = new Detail("Корпус GiNZZU GL5501", "Корпуса", 30);
+            details[3] = new Detail("Корпус CoolerMaster MasterBox Q300P MCB-Q300P-KANN-S02 черный 2", "Корпуса", 115);
+            details[4] = new Detail("Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1", "Корпуса", 30);
+            details[5] = new Detail("Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 2", "Корпуса", 1211);
+            details[6] = new Detail("Видеокарта-GIGABYTE-GeForce-RTX-2080-Super-AORUS-WATERFORCE-WB", "Видеокарты", 60000);
+            details[7] = new Detail("Видеокарта KFA2 GeForce GTX 1080 Ti HOF", "Видекарты", 55000);
+            details[8] = new Detail("Видеокарта Gigabyte GeForce GTX 1080 Ti AORUS XTREME WB", "Видекарты", 85000);
+            details[9] = new Detail("Видеокарта Gigabyte GeForce GTX 1060 WF O", "Видекарты", 20000);
+            details[10] = new Detail("Видеокарта MSI GeForce RTX 2060 GAMING", "Видекарты", 20000);     
 
-
-            details[0] = new Detail();
-            details[0].category = "Корпуса";
-            details[0].name = "Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1";
-            //details[0].picture = pictureBox1;
-
-            details[1] = new Detail();
-            details[1].category = "Корпуса";
-            details[1].name = "Корпус GiNZZU GL5501";
-            //details[1].picture = pictureBox2;
-
-            details[2] = new Detail();
-            details[2].category = "Корпуса";
-            details[2].name = "Корпус GiNZZU GL550 2";
-            //details[2].picture = pictureBox3;
-
-            details[3] = new Detail();
-            details[3].category = "Корпуса";
-            details[3].name = "Корпус CoolerMaster MasterBox Q300P MCB-Q300P-KANN-S02 черный 2";
-            //details[3].picture = pictureBox4;
-
-            details[4] = new Detail();
-            details[4].category = "Корпуса";
-            details[4].name = "Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1";
-            //details[4].picture = pictureBox5;
-
-            details[5] = new Detail();
-            details[5].category = "Корпуса";
-            details[5].name = "Корпус DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 2";
-            //details[5].picture = pictureBox5;
-
-            details[6] = new Detail();
-            details[6].category = "Видеокарты";
-            details[6].name = "Видеокарта-GIGABYTE-GeForce-RTX-2080-Super-AORUS-WATERFORCE-WB";
-            //details[6].picture = pictureBox6;
-
-            details[7] = new Detail();
-            details[7].category = "Видеокарты";
-            details[7].name = "Видеокарта KFA2 GeForce GTX 1080 Ti HOF";
-            //details[3].picture = pictureBox7;
-
-            details[8] = new Detail();
-            details[8].name = "Видеокарта Gigabyte GeForce GTX 1080 Ti AORUS XTREME WB";
-            //details[8].picture = pictureBox8;
-
-            details[9] = new Detail();
-            details[9].name = "Видеокарта Gigabyte GeForce GTX 1060 WF OC";
-            //details[9].picture = pictureBox9;
-
-            details[10] = new Detail();
-            details[10].name = "RTX 2060 D6";
-            //details[10].picture = pictureBox10;
-
-            details[11] = new Detail();
-            details[11].name = "Видеокарта MSI GeForce RTX 2060 GAMING";
-            //details[11].picture = pictureBox11;
-
-
-            
             int x = 55;
             int y = 120;  
-            for (int i = 0; i < 12 ; i++)
+            for (int i = 0; i < 11 ; i++)
             {
                 PictureBox pb = new PictureBox();
                 pb.Location = new Point(x, y);
@@ -102,7 +63,9 @@ namespace WindowsN1VeX
                 {
                     pb.Load("../../Resources/" + details[i].category + "/" + details[i].name + ".jpg");
                 }
-                catch (Exception) { }
+                catch (Exception ex) {
+                   
+                }
                 details[i].picture = pb;
                 Controls.Add(pb);
 
@@ -128,7 +91,7 @@ namespace WindowsN1VeX
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 11; i++)
             {
              details[i].picture.Visible = false;
             if (textBox1.Text != "" &&
