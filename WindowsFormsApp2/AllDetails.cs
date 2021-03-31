d682f5ca5f48f024a201ce62a327fe3aea28809d
Артём Плешаков <rpdelta132@gmail.com>
2021-03-31 17:01:13 +0300

    менял массив и доделал перевод

--- WindowsFormsApp2/AllDetails.cs
+++ WindowsFormsApp2/AllDetails.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace WindowsN1VeX
 {  
@@ -38,41 +40,13 @@ namespace WindowsN1VeX
         public static Dictionary<string, string> RusWords = new Dictionary<string, string>();
         public static Dictionary<string, string> EngWords = new Dictionary<string, string>();
 
-        
 
         /// <summary>
         /// Заполнить все детали
         /// </summary>
         public static void FillDetails()
         {
-            details_List.Add(new Detail("DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1", "Корпуса", 0));
-            details_List.Add(new Detail("GiNZZU GL5501", "Корпуса", 0));
-            details_List.Add(new Detail("GiNZZU GL5501", "Корпуса", 30));
-            details_List.Add(new Detail("CoolerMaster MasterBox Q300P MCB-Q300P-KANN-S02 черный 2", "Корпуса", 115));
-            details_List.Add(new Detail("DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 1", "Корпуса", 30));
-            details_List.Add(new Detail("DEEPCOOL MATREXX 55 V3 ADD-RGB 3F 2", "Корпуса", 1211));
-            details_List.Add(new Detail("GIGABYTE-GeForce-RTX-2080-Super-AORUS-WATERFORCE-WB", "Видеокарты", 60000));
-            details_List.Add(new Detail("KFA2 GeForce GTX 1080 Ti HOF", "Видеокарты", 55000));
-            details_List.Add(new Detail("Gigabyte GeForce GTX 1080 Ti AORUS XTREME WB", "Видеокарты", 85000));
-            details_List.Add(new Detail("Gigabyte GeForce GTX 1060 WF OC", "Видеокарты", 20000));
-            details_List.Add(new Detail("GTX 1660 SUPER", "Видеокарты", 20000));
-            details_List.Add(new Detail("Gigabyte GeForce GTX 1080 Ti FOUNDER EDITION", "Видеокарты", 20000));
-            details_List.Add(new Detail("RX 5500 XT", "Видеокарты", 20000));
-            details_List.Add(new Detail("GTX 1650", "Видеокарты", 20000));
-            details_List.Add(new Detail("GTX 1660 SUPER", "Видеокарты", 20000));
-            details_List.Add(new Detail("Intel i9-10900k", "Процессоры", 20000));
-            details_List.Add(new Detail("Ryzen 5 3600XT", "Процессоры", 20000));
-            details_List.Add(new Detail("Ryzen 7 3700", "Процессоры", 20000));
-            details_List.Add(new Detail("Intel I9-9900k", "Процессоры", 20000));
-            details_List.Add(new Detail("Intel I7-9700k", "Процессоры", 20000));
-            details_List.Add(new Detail("Intel I3-9100F", "Процессоры", 20000));
-            details_List.Add(new Detail("Intel i5-9400F", "Процессоры", 20000));
-            details_List.Add(new Detail("be quiet! SYSTEM POWER 9 550W", "Блок питания", 20000));
-            details_List.Add(new Detail("Chieftec 750W", "Блок питания", 20000));
-            details_List.Add(new Detail("CoolerMaster MWE 550 WHITE - V2", "Блок питания", 20000));
-            details_List.Add(new Detail("ZALMAN MegaMax", "Блок питания", 20000));
-            details_List.Add(new Detail("Toughpower GX1 500W", "Блок питания", 20000));
-            details_List.Add(new Detail("Deepcool DQ650", "Блок питания", 20000));
+            
 
             RusWords.Add("Название", "Название");
             EngWords.Add("Название", "Name");
@@ -81,6 +55,12 @@ namespace WindowsN1VeX
             RusWords.Add("Поиск", "Поиск");
             EngWords.Add("Поиск", "Search");
 
+            string[] lines = File.ReadAllLines("../../../Комплектующие.txt");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(new string[] { ", " }, StringSplitOptions.None);
+                details_List.Add(new Detail(parts[0], parts[1], Convert.ToInt32(parts[2])));
+            }
 
             for (int i = 0; i < details_List.Count; i++)
             {
